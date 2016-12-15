2822ea67449b5172e04dab84d3211ecd3b5a634f
Piotr Glazar <piotr.glazar@gmail.com>
2016-12-15 20:41:25 +0100

    gitignore update and initial project setup

---
+++ src/main/java/com/piotrglazar/WellPaidWorkApplication.java
@@ -0,0 +1,12 @@
+package com.piotrglazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellPaidWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellPaidWorkApplication.class, args);
+	}
+}
