9c035ec524317093fa9b71cc207c14ec8a0766d6
Piotr Glazar <piotr.glazar@gmail.com>
2017-01-17 21:32:06 +0100

    use spring profiles to clean up application properties

--- src/test/java/com/piotrglazar/wellpaidwork/WellPaidWorkApplicationTests.java
+++ src/test/java/com/piotrglazar/wellpaidwork/WellPaidWorkApplicationTests.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -26,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class WellPaidWorkApplicationTests {
 
     @Value("${sense.of.the.universe}")
